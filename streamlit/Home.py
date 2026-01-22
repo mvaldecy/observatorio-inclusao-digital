a50ccb88fdd2ed4b73b9671ad9d36bb2b830bc8f
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2026-01-22 11:23:14 -0300

    feat: update homepage title and icon, add pages configuration for CETIC analysis

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Observatório de Inclusão Digital",
-    page_icon="🌐",
+    page_title="Home",
+    page_icon="🏠",
     layout="wide"
 )
 
